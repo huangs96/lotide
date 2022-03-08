b77e2fed5759b4f41fd2b9a95b035c3dbefedc1d
Stephen Huang <stephen_1996@live.ca>
2022-03-08 07:47:55 +0000

    head check

---
+++ head.js
@@ -0,0 +1,17 @@
+let head = function (array) {
+	if(!Array.isArray(array) || array.length == 0) {
+		return null;
+	}
+
+	return array[0];
+}
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]));
