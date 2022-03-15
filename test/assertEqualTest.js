d97adb382d09e6aa72ba588a59c3178498027bde
Stephen Huang <stephen_1996@live.ca>
2022-03-15 00:36:45 +0000

    created two test files for head.js and assertEqual.js

---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const {assertEqual} = require('../assertEqual')
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(3, 3);
+assertEqual(3, 2);
---
+++ test/headTest.js
@@ -0,0 +1,5 @@
+const {assertEqual} = require('../assertEqual')
+
+assertEqual(5, 5);
+assertEqual("Hello", "Hello");
+assertEqual(null, null);
